1f3d94605dbba0ca9cd4f7e45b951a61e210c4b8
swingcip-creator <swingcip@gmail.com>
2026-07-18 21:21:18 +0300

    Delete radar/web directory

--- radar/web/src/state/storage.ts
+++
@@ -1,1 +0,0 @@
-export { default as kReduxPersistLocalStorage } from "redux-persist/lib/storage";
--- radar/web/src/ui/utils/force-render.ts
+++
@@ -1,8 +0,0 @@
-import { useCallback, useState } from "react";
-
-export function useForceUpdate(): () => void {
-    const [_state, setState] = useState<number>(0);
-    return useCallback(() => {
-        setState(performance.now());
-    }, [setState]);
-}
